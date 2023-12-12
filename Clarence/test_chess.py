107275418ac45b07b2c783a318b76412613b74cd
Foxed972 <clarencegaudemet@gmail.com>
2023-12-12 17:40:40 +0100

    test_chess

--- Clarence/test_chess.py
+++ Clarence/test_chess.py
@@ -1,11 +1,13 @@
 import time
 import chess
+from random import*
 
 board=chess.Board()
 
 legal_moves=board.legal_moves
+legal_moves=list(legal_moves)
 
 while True :
-    board.push(list(legal_moves)[0])
+    board.push(legal_moves[randint(0,len(legal_moves))])
     print(board)
     time.sleep(1)
